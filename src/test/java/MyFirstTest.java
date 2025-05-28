32eec7de90f99e5c688fecfac79727efeb0a098b
CamAGarzonG <caagarzonga@gmail.com>
2025-05-28 00:52:04 -0500

    Add test config file

--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,16 +1,17 @@
+import config.TestConfig;
 import org.junit.Test;
-
 import static io.restassured.RestAssured.*;
-import static io.restassured.RestAssured.given;
 
-public class MyFirstTest {
+
+public class MyFirstTest extends TestConfig {
 
     @Test
     public void myFirstTest() {
         given()
                 .log().all()
         .when()
-                .get("https://videogamedb.uk/api/videogame")
+                //.get("https://videogamedb.uk/api/videogame")
+                .get("/videogame")
         .then()
                 .log().all();
 
